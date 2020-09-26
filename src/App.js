a4b40f4c623d0ba5a3c907c93e6a37feced983f0
Dylan Theriot <dylantheriot@gmail.com>
2020-09-26 11:56:35 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="text-2xl">Howdy ShellHacks!</div>
+    </div>
+  );
+}
+
+export default App;
