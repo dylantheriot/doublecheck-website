9a7628513458452e17bf9aaa55af642b53c86725
Dylan Theriot <dylantheriot@gmail.com>
2020-09-27 04:36:11 -0500

    ye

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import CreateSessionPage from './components/create_session_page/CreateSessionPage';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import DashboardPage from './components/dashboard_page/DashboardPage';
 
 function App() {
   return (
     <Router>
-      <Route exact path='/' component={CreateSessionPage} />
+    <Switch>
+    <Route exact path='/' component={CreateSessionPage} />
       <Route exact path='/dashboard' component={DashboardPage} />
+    </Switch>
+
     </Router>
   );
 }
